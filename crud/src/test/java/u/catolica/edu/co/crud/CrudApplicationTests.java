ac8efca27df05bdd35339291fa0f92894c17d9b6
Johan Cifuentes <110059991+JohanCifuentes03@users.noreply.github.com>
2024-09-17 23:21:23 -0500

    first commit

---
+++ crud/src/test/java/u/catolica/edu/co/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package u.catolica.edu.co.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
